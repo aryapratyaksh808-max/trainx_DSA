21083ce682d3e395862b4125149afc5909ba75e5
tony Stark <aryapratyaksh808@gmail.com>
2026-09-24 21:22:03 +0530

    s

---
+++ Pyramid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Pyramid {
+     static void pyramid(int n){
+          for(int i=1;i<=n;i++){
+               for (int j=1;j<=n-i;j++){
+                    System.out.print("  ");
+               }
+               for(int j=1;j<=i;j++){
+                    System.out.print(" *  ");
+               }
+               System.out.println();
+          }
+     }
+          static void InvertedPyramid(int n){
+          for(int i=n;i>=1;i--){
+               for(int j=1;j<=n-i;j++){
+                    System.out.print("  ");
+               }
+               for(int j=1;j<=i;j++){
+                    System.out.print(" *  ");
+               }
+               System.out.println();
+          }
+     }
+     public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the number: ");
+     int n  = sc.nextInt();
+     pyramid(n);
+     InvertedPyramid(n);
+     
+     }
+     
+}
