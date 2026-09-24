91c6fbc7c42c05b81c41efecec08a223033f5529
tony Stark <aryapratyaksh808@gmail.com>
2026-09-24 13:43:38 +0530

    first comit

---
+++ optimusPrime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class optimusPrime{
+     public static boolean isPrime(int n){
+          if(n<=1){
+               return false;
+          }
+          for(int i=2;i<n;i++){
+               if(n%i==0){
+                    return false;
+               }
+              
+          }
+          return false;
+     }
+     public static void printPrime(int n){
+          for(int i=2;i<=n;i++)
+          {
+               if(isPrime(i)){
+                    System.out.print(i+" ");
+               }
+          }
+     }
+     public static void main(String[] args)
+     {
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     printPrime(n); 
+     }
+
+}    
