a6ef262ad2d3b00f93859d02268445559239b60a
tony Stark <aryapratyaksh808@gmail.com>
2026-09-24 22:56:12 +0530

    hollow square with spaces

---
+++ HollowSquare.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class HollowSquare {
+     static void hollowSquare(int n){
+               for (int i = 1; i <= n; i++) { 
+ 
+            for (int j = 1; j <= n; j++) { 
+ 
+                if (i == 1 || i == n || j == 1 || j == n) { 
+                    System.out.print("*"); 
+                } else { 
+                    System.out.print(" "); 
+                } 
+                System.out.print("   ");
+            } 
+ 
+            System.out.println(); 
+        } 
+     }
+     public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+          System.out.print("Enter the number: ");
+          int n= sc.nextInt();
+
+          hollowSquare(n);
+     }
+}
